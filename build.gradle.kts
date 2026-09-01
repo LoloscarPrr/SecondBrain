92c00e79803eb6bcd6fa09f488d5db916c118191
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-09-01 10:26:41 -0400

    build: prepare Firebase Crashlytics plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@ plugins {
     id("com.android.application") version "9.3.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("com.google.devtools.ksp") version "2.3.10" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
+    id("com.google.firebase.crashlytics") version "3.0.8" apply false
 }
